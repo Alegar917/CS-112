41dc4f7a48cdb26c46ddb0c9b381f50a56fc8c10
Alejandro Garcia <argarcia6@dons.usfca.edu>
2018-04-18 15:17:14 -0700

    Add files via upload

---
+++ HangmanAI1.java
@@ -0,0 +1,37 @@
+ public class HangmanAI1 extends Hangman{
+ 	HangmanPlayer player=new HangmanPlayer();
+ 	int i;
+ 	public String getPhrase(){
+		String p=incPhrase(this.i);
+		return p;
+	}
+ 	public String getGuess(){
+		System.out.print("Guess a Character:");
+		String guess=player.guess(hiddenPhrase);
+		return guess;
+	}
+	public String id(){
+		return "playerAi";
+	}
+	public static void main(String[] args){
+		HangmanAI1 hang=new HangmanAI1();
+		GamesRecord rec=new GamesRecord();
+		hang.readPhrases();
+		String p=hang.getPhrase();
+		hang.play(p);
+		rec.add(hang);
+		int i=1;
+		while(i<hang.phraseList.size()){
+			hang.i=i;
+			p=hang.getPhrase();
+			hang=new HangmanAI1();
+			hang.player=new HangmanPlayer();
+			hang.readPhrases();
+			hang.play(p);
+			rec.add(hang);
+			i++;
+		}
+		System.out.println("Average score:"+rec.average());
+		rec.highGameList(3);
+	}
+ }
